40777901ebc4903db160c286b18f5bdde0e26539
Kyle Delaney <khdelaney99@gmail.com>
2024-10-16 10:32:49 -0400

    add dynamic population

--- react-tutorial/src/components/ListGroup.tsx
+++ react-tutorial/src/components/ListGroup.tsx
@@ -1,5 +1,24 @@
 function ListGroup() {
-    return <h1>List Group</h1>
+
+    var items = [
+        "London",
+        "New York",
+        "Philadelphia",
+        "Istanbul",
+        "Barcelona"
+    ]
+
+    return (
+        <>
+            <h1>Cities</h1>
+            {items.length === 0 && <p>no items found</p>}
+            <ul className="list-group">
+                {items.map(item => (
+                    <li className="list-group-item" key={item}>{item}</li>
+                ))}
+            </ul>    
+        </>
+     );
 }
 
 export default ListGroup
